a1d23ace05862b0a9c6d6375293796eda6ec53f8
haamid10 <apdixamiid919@gmail.com>
2022-05-10 12:54:12 +0300

    floating nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -7,15 +7,15 @@ import { InboxInIcon } from '@heroicons/react/outline'
 
 const Nav = () => {
   return (
-    <div  className='ml-96  backdrop-blur-lg bg-secondary w-72 h-12 bottom-4 bg-light absolute fixed '>
+    <div  className=' bottom-8 fixed top-2  '>
   
    
-      <div className=' ml-80  backdrop-blur-lg rounded-lg mb-16 flex flex-row   gap-5 blur-none opacity-100 top-2 relative '>
-      <a className=' bg-transparent' href='#home'> <HomeIcon  className=" w-8 h-16 "/> </a>
-      <a  className=' bg-transparent'  href='#about'> <UserIcon  className=" blur-none w-8 h-16"/> </a>
-      <a   className=' bg-transparent'  href='#expereince'> <BookOpenIcon  className=" w-8 h-16"/> </a>
-      <a  className=' bg-transparent'   href='#footer'> <CogIcon  className=" w-8 h-16"/> </a>
-      <a   className=' bg-transparent'  href='#contact'> <InboxInIcon  className="   w-8 h-16"/> </a>
+      <div className='backdrop-blur-lg bg-nav rounded-lg  flex flex-row  justify-center  gap-5 blur-none opacity-100 top-2 '>
+      <a className=' bg-transparent' href='#home'> <HomeIcon   className="text-light w-8 h-16 "/> </a>
+      <a  className=' bg-transparent'  href='#about'> <UserIcon  className=" text-light w-8 h-16"/> </a>
+      <a   className=' bg-transparent'  href='#expereince'> <BookOpenIcon  className="  text-light w-8 h-16"/> </a>
+      <a  className='text-light bg-transparent'   href='#footer'> <CogIcon  className=" w-8 h-16"/> </a>
+      <a   className=' bg-transparent'  href='#contact'> <InboxInIcon  className="text-light   w-8 h-16"/> </a>
       </div>
       
     
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,24 +3,24 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    colors:{
+    colors: {
       transparent: 'transparent',
       secondary: '#1f1f38',
-     
-       variant: '#2c2c6c',
-       white: '#fff',
-       primary:' #4db5ff',
-       primaryv:' rgba(77,181,255,0.4)',
-       light:  'rgba(255,255,255, 0.6)',
+
+      variant: '#2c2c6c',
+      white: '#fff',
+      primary: ' #4db5ff',
+      primaryv: ' rgba(77,181,255,0.4)',
+      light: 'rgba(255,255,255, 0.6)',
+      nav: 'rgba(0,0,0,0.3)',
     },
-  
+
     extend: {
-   fontFamily:{
-    'Poppins': 'sans-serif'
-   }
+      fontFamily: {
+        'Poppins': 'sans-serif'
+      }
     },
   },
   plugins: [],
 }
- // 
-     
+ //
